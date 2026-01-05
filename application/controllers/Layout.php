c5ae3092a94b81fde26e4d05729b15d8c5653631
khamdialfin <alfinpml220@gmail.com>
2026-01-05 14:18:27 +0700

    memecah kode template boostrap

--- application/controllers/Layout.php
+++ application/controllers/Layout.php
@@ -1,15 +1,17 @@
 <?php
- defined('BASEPATH') OR exit('No direct script access allowed');
- 
- class Layout extends CI_Controller {
- 
-    public function index()
-    {
-        $this->load->view('Admin/header');
-    }
- 
- }
- 
- /* End of file Controllername.php */
- 
-?>
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Layout extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->load->view('Admin/header');
+		$this->load->view('Admin/navbar');
+		$this->load->view('Admin/main');
+		$this->load->view('Admin/sidebar');
+		$this->load->view('Admin/footer');
+	}
+}
+
+/* End of file Controllername.php */
